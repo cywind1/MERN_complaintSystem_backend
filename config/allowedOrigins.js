690c69a8971de2ac54748b5ed95950485887973f
cywind1 <chingjusttest@gmail.com>
2023-05-18 15:18:31 +0200

    13_deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 // 2.4.1 CORS
-const allowedOrigins = ["http://localhost:3000"];
+// const allowedOrigins = ["http://localhost:3000"];
+// 13.3
+const allowedOrigins = ["https://susie-complaints-system.onrender.com"];
 
 module.exports = allowedOrigins;
 
